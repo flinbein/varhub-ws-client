9370a98949b92ef8fa0f2f6deba9ddf5590e2caf
dpohvar <dpohvar@gmail.com>
2023-11-24 23:27:53 +0200

    add bin and text modules 1.0.13

--- src/types/ModuleDescription.d.ts
+++ src/types/ModuleDescription.d.ts
@@ -1,4 +1,4 @@
-export type ModuleDescription = ModuleJsDescription | ModuleJsonDescription;
+export type ModuleDescription = ModuleJsDescription | ModuleJsonDescription | ModuleTextDescription | ModuleBinDescription;
 export interface ModuleJsDescription {
 	type: "js",
 	source?: string,
@@ -8,4 +8,13 @@ export interface ModuleJsDescription {
 export interface ModuleJsonDescription {
 	type: "json",
 	source: string
+}
+
+export interface ModuleTextDescription {
+	type: "text",
+	source: string
+}
+export interface ModuleBinDescription {
+	type: "bin",
+	source: Uint8Array
 }
